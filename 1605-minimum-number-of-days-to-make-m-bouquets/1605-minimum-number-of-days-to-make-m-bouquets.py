aeae6e9fe472c7adf5371f016ea9f6261ae01057
Roshiq2 <166851041+Roshiq2@users.noreply.github.com>
2024-08-23 16:27:35 +0530

    Time: 896 ms (32.68%), Space: 29.8 MB (87.46%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        def possible(arr, day, m, k):
+            n = len(arr)  # size of the array
+            cnt = 0
+            noOfB = 0
+            # count the number of bouquets
+            for i in range(n):
+                if arr[i] <= day:
+                    cnt += 1
+                else:
+                    noOfB += cnt // k
+                    cnt = 0
+            noOfB += cnt // k
+            return noOfB >= m
+
+        def roseGarden(arr, k, m):
+            val = m * k
+            n = len(arr)  # size of the array
+            if val > n:
+                return -1  # impossible case
+            # find maximum and minimum
+            mini = float('inf')
+            maxi = float('-inf')
+            for i in range(n):
+                mini = min(mini, arr[i])
+                maxi = max(maxi, arr[i])
+
+            # apply binary search
+            low = mini
+            high = maxi
+            while low <= high:
+                mid = (low + high) // 2
+                if possible(arr, mid, m, k):
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return low
+        return roseGarden(bloomDay,k,m)
