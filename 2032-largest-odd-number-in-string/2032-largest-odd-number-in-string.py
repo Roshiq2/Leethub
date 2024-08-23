9258640862f45b5e2fc6a686e78ca1a6e0bb934c
Roshiq2 <166851041+Roshiq2@users.noreply.github.com>
2024-08-23 23:52:32 +0530

    Time: 42 ms (90.3%), Space: 17.7 MB (52.57%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        ros=""
+        for i in range(len(num)-1,-1,-1):
+            if (ord(num[i])-ord('0'))%2!=0:
+                return num[:i+1]
+        return ""
+        
