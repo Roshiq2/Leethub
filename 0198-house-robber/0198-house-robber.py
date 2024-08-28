ad01ef3f83d3481e7d73949063de179b6a67b20b
Roshiq2 <166851041+Roshiq2@users.noreply.github.com>
2024-08-28 14:47:02 +0530

    Time: 31 ms (85.11%), Space: 16.7 MB (5.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,9 +6,11 @@ class Solution:
                 return 0
             if n==0:
                 return nums[n]
+            if dp[n]!=0:
+                return dp[n]
             pick=nums[n]+helper(n-2)
             notpick=helper(n-1)
             return max(pick,notpick)
-
         n=len(nums)
+        dp=[0 for i in range(n)]
         return helper(n-1)     
