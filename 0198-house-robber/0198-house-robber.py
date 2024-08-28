f10685ec269cf99f00d4c64661b5de8a610da2d6
Roshiq2 <166851041+Roshiq2@users.noreply.github.com>
2024-08-28 14:49:44 +0530

    Time: 42 ms (15.56%), Space: 16.5 MB (68.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @functools.cache
         def helper(n):
             if n<0:
                 return 0
             if n==0:
                 return nums[n]
-            if dp[n]!=0:
+            if dp[n]!=-1:
                 return dp[n]
             pick=nums[n]+helper(n-2)
             notpick=helper(n-1)
-            return max(pick,notpick)
+            dp[n]=max(pick,notpick)
+            return dp[n]
+
         n=len(nums)
-        dp=[0 for i in range(n)]
+        dp=[-1 for i in range(n)]
         return helper(n-1)     
