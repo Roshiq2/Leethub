bcb09c04225aea37410544714ba80d697c4fd965
Roshiq2 <166851041+Roshiq2@users.noreply.github.com>
2026-08-25 16:29:00 +0530

    Time: 4 ms (99.71%), Space: 45.4 MB (64%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -1,29 +1,55 @@
 class Solution {
     public String rankTeams(String[] votes) {
-      Map<Character, int[]> map = new HashMap<>();
-      int l = votes[0].length();
-      for(String vote : votes){
-        for(int i = 0; i < l; i++){
-          char c = vote.charAt(i);
-          map.putIfAbsent(c, new int[l]);
-          map.get(c)[i]++;
+        int[][]pos= new int[26][votes[0].length()];
+        for(int i =0;i<votes.length;i++){
+            for(int j=0;j<votes[i].length();j++){
+                pos[votes[i].charAt(j)-'A'][j]++;
+            }
         }
-      }
-      
-      List<Character> list = new ArrayList<>(map.keySet());
-      Collections.sort(list, (a,b) -> {
-        for(int i = 0; i < l; i++){
-          if(map.get(a)[i] != map.get(b)[i]){
-            return map.get(b)[i] - map.get(a)[i];
-          }
+        StringBuilder sb = new StringBuilder();
+        for(int i =0;i<votes[0].length();i++){
+            //fr iterating over the pos
+            int[]check= new int[26];
+            Arrays.fill(check,1);
+            boolean chek= false;
+            for(int j=0;j<votes[0].length();j++){
+                            int c=0;
+
+              //for iterating over the columns now 
+              int m =0;
+              for(int z=0;z<26;z++){
+                if(check[z]==1)m= Math.max(m,pos[z][j]);}
+              for(int z=0;z<26;z++){
+                if(pos[z][j]!=m)check[z]=0;
+                else c++;
+              }
+              if(c==1){
+                int po=0;
+                for(int z=0;z<26;z++)if(check[z]==1)po=z;
+                for (int k = 0; k < votes[0].length(); k++) {
+    pos[po][k] = 0;
+}     
+chek=true;
+                sb.append((char)('A'+po));
+                  String g= sb.toString();
+                        if(g.length() == votes[0].length())return g;
+                break;
+              }
+            }
+            if(!chek){
+                for(int z=0;z<26;z++){
+                    if(check[z]==1){
+                        sb.append((char)('A'+z));
+                        String g= sb.toString();
+                        if(g.length() == votes[0].length())return g;
+                        for (int k = 0; k < votes[0].length(); k++) {
+    pos[z][k] = 0;
+}
+                    }
+                }
+            }
         }
-        return a - b;
-      });
-      
-      StringBuilder sb = new StringBuilder();
-      for(char c : list){
-        sb.append(c);
-      }
-      return sb.toString();
+
+    return sb.toString();
     }
 }
